a90293f48fb1eab6065e39c37c16ba710cbf1bcf
heatmyhome-ninja <94574200+heatmyhome-ninja@users.noreply.github.com>
2022-01-13 21:57:30 +0000

    Delete pkg directory

--- pkg/rust_simulator_bg.wasm.d.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function run_simulation(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number, l: number, m: number, n: number, o: number, p: number): void;
-export function __wbindgen_add_to_stack_pointer(a: number): number;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number): number;
-export function __wbindgen_free(a: number, b: number): void;
